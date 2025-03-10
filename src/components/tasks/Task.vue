9c23d631102971e2331e8febbae77cdcb4f4c9b7
BabarAslamm <babaraslam825@gmail.com>
2025-03-10 21:59:13 +0500

    using computed properties for displaying completed and uncompleted tasks

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,8 +1,8 @@
 <template>
     <li class="list-group-item py-3">
         <div class="d-flex justify-content-start align-items-center">
-            <input class="form-check-input mt-0 completed" type="checkbox" />
-            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+            <input class="form-check-input mt-0" :class="completedClass" :checked="task.is_completed"  type="checkbox" />
+            <div class="ms-2 flex-grow-1" :class="completedClass" title="Double click the text to edit or remove">
                 <!-- <div class="relative">
                     <input class="editable-task" type="text" />
                 </div> -->
@@ -19,20 +19,22 @@
             <button class="btn btn-sm btn-circle btn-outline-danger">
                 <IconTrash />
             </button>
-            
+
         </div>
     </li>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
 import IconPencil from '@/components/icons/IconPencil.vue';
 import IconTrash from '@/components/icons/IconTrash.vue';
 
-defineProps({
+const props = defineProps({
     task: {
         type: Object
     }
 })
+
+const completedClass = computed(() => props.task.is_completed ? 'completed': '');
 
 </script>
--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -8,12 +8,17 @@
                         <input type="text" class="form-control form-control-lg padding-right-lg"
                             placeholder="+ Add new task. Press enter to save." />
                     </div>
-                    <!-- List of tasks -->
-                    <div class="card mt-2">
-                        <ul class="list-group list-group-flush">
-                            <Task v-for="task in tasks" :task="task" :key="task.id" />
-                        </ul>
-                    </div>
+
+                    <!-- List Of Uncompleted Tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+                    
+                    <!-- List Of Completed Tasks -->
+                    <Tasks :tasks="completedTasks" />
+
+
+
+
+
                 </div>
             </div>
         </div>
@@ -21,19 +26,23 @@
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue';
+    import { onMounted, ref, computed } from 'vue';
     import { allTasks } from '../http/task-api';
-    import Task from '@/components/tasks/Task.vue'
+
+    import Tasks from '@/components/tasks/Tasks.vue';
 
     const tasks = ref([]);
 
     onMounted( async () => {
 
-        const { data } = await allTasks()
+        const { data } = await allTasks();
 
         tasks.value = data.data;
 
     });
+
+    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
 
 </script>
 
